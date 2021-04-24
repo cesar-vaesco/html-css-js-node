6c4ce2b006e5676a5d151475322448170ec860b4
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-04-23 23:05:17 -0500

    JS - introducción a funciones

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+
+function iterar(arg1) {
+
+
+    for (let i = 0; i < arg1.length; i++) {
+
+        console.log(arg1[i])
+
+    }
+}
+
+const numeros = [1, 2, 'hola', 4, 5];
+const nombres = ['Pedro', 'Juan', 'Felipe', 'Feliz', 'Triste'];
+
+iterar( numeros );
+iterar( nombres );
