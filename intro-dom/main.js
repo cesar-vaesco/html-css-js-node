165bdf743ca33eebb2795d3b2a488b65c5aa4e89
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-04-26 15:00:34 -0500

    DOM - manipulando el texto a traves de innerText/ innerHTML

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,18 +1,10 @@
-/**
- * window.onload ayuda a que nuestro archivo javascript se cargue despues
- * de que el navegador cargue el archivo / archivos html
- */
 
 window.onload = () => {
 
-    /* GetElementById permite obtener una referencia el contenido que tengan envuelto
-       todas las etiquetas identificadas con el Id*/
     const parrafo = document.getElementById('text');
-
-    /*
-        innerHTML/ innerText permite conocer el contenido
-        señalado con la función getElementById
-    */
-    console.log(parrafo.innerHTML);
+    const parrafo2 = document.getElementById('text2');
+    /*Pasando texto e forma dinamica a la vista*/
+    parrafo.innerHTML="Texto actualizado....";
+    parrafo2.innerText="Segundo texto actualizado";
 
 }
