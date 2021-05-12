a9bc342b2698d63d0fcae43ee58d2184c4ccaa56
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-05-12 12:21:53 -0500

    DOM - TODO: Agregando elementos HTML a una lista de tareas

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,3 +1,5 @@
+//Crea un arreglo donde se van almacenar las tareas
+const todos = [];
 
 window.onload = () => {
 
@@ -10,7 +12,19 @@ window.onload = () => {
         const todoText = todo.value;
         todo.value = '';
 
-        console.log(todoText);
+        //Agregar treas al arreglo todos
+        todos.push(todoText);
+        //Apuntamos a la referencia donde se van agragar las tareas
+        const todoList = document.getElementById('todo-list');
+        // Se limpia el arreglo con la iguiente instrucción
+        // para evitar que el ciclo for vuelva  pintar las tareas y antes
+        // agregadas
+        todoList.innerHTML = '';
+        //El ciclo for permite agregar la lista no ordenadas de tareas
+        for (let i = 0; i < todos.length; i++) {
+            todoList.innerHTML += '<li> ' + todos[i] + '</li>';
+        }
+        console.log(todos);
     }
 
 
