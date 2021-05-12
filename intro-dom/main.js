27c6cd0585b5c7f30ce6a2e9ba98a73347fcd2ad
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-05-12 14:15:03 -0500

    DOM - TODO: función render - actualizando registros en arreglo al ser borrados elementos del mismo

--- intro-dom/main.js
+++ intro-dom/main.js
@@ -1,6 +1,32 @@
 //Crea un arreglo donde se van almacenar las tareas
 const todos = [];
 
+const render = () =>{
+    //Apuntamos a la referencia donde se van agragar las tareas
+    const todoList = document.getElementById('todo-list');
+
+    //Map crea un nuevo arreglo a partir de un arreglo ya existente
+    const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+
+    //El método join() une todos los elementos de una matriz (o un objeto similar a una matriz) en una cadena y devuelve esta cadena.
+    todoList.innerHTML = todosTemplate.join('');
+
+    //selccionamos todos lo elementos
+    const elementos = document.querySelectorAll('#todo-list li');
+
+    //iteramos los elementos
+    elementos.forEach((elemento, i) => {
+        //Agregamos un evento al elemento escuchando el evento click
+        elemento.addEventListener('click', () => {
+
+            elemento.parentNode.removeChild(elemento);
+            todos.splice(i, 1);
+            render();
+            console.log(todos, i);
+        });
+    });
+}
+
 window.onload = () => {
 
     const form = document.getElementById("todo-form");
@@ -14,28 +40,9 @@ window.onload = () => {
 
         //Agregar treas al arreglo todos
         todos.push(todoText);
-        //Apuntamos a la referencia donde se van agragar las tareas
-        const todoList = document.getElementById('todo-list');
 
-        //Map crea un nuevo arreglo a partir de un arreglo ya existente
-        const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+        render();
 
-        //El método join() une todos los elementos de una matriz (o un objeto similar a una matriz) en una cadena y devuelve esta cadena.
-        todoList.innerHTML = todosTemplate.join('');
-
-        //selccionamos todos lo elementos
-        const elementos = document.querySelectorAll('#todo-list li');
-
-        //iteramos los elementos
-        elementos.forEach((elemento, i) =>{
-            //Agregamos un evento al elemento escuchando el evento click
-            elemento.addEventListener('click', () =>{
-
-                elemento.parentNode.removeChild(elemento);
-                todos.splice(i, 1);
-                console.log(todos, i);
-            })
-        })
     }
 
 
