1819ac7ec1d265391cce0a9283152e216597bc80
Cesar-Dev <cesarvargasescorcia@gmail.com>
2021-05-12 22:47:49 -0500

    Operador spread: expanción de arrays

---
+++ funcionalidades-javascript/main.js
@@ -0,0 +1,20 @@
+
+const a = { b:1 }
+
+const b = a;
+
+console.log( a === b);
+
+/**
+ * Spread operator de arrays
+
+Este operador permite que los elementos de un array
+se expandan y, de esta manera, podemos añadir un array dentro de otro
+sin que el resultado sean arrays anidados, si no un
+único array al que se han añadido nuevos valores.
+
+*/
+
+//el objeto c es un objeto independiebnte de a aunque
+// contengan algunos elementos en común o que c fue creado basandose en a
+const c = { ...a}
